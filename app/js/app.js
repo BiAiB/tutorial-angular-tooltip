735bd5d3fc4f9343211e8a44a94f077365497c3c
Rodolphe Gohard <rodolphe@gohard.fr>
2014-05-27 10:47:47 +0200

    directive tpHelp

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,24 @@
-angular.module('tutorial-contextualhelp', [
-    'ngResource'
-]);
+!function() {
 
+	var moudule = angular.module( 'tutorial-contextualhelp', [
+		'ngResource'
+	] );
+
+	/**
+	 * bulles d'aide
+	 */
+	moudule.directive('tpHelp', function() {
+		var directiveConfig = {
+			replace: true,
+			restrict: 'AE',
+			transclude: true,
+			templateUrl: 'partials/directives/helpbubble.html',
+			scope: {
+				src: '@src'
+			}
+		};
+		return directiveConfig;
+	});
+
+}();
 
